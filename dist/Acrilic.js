acb3f179470e7d4e43fce83318cb5c6f4b2ac7e6
Karlisson Bezerra <contact@hacktoon.com>
2016-06-01 00:45:45 -0300

    Move map file to base module

--- dist/Acrilic.js
+++ dist/Acrilic.js
@@ -137,6 +137,50 @@ ac.export("graphics", function(){
 	};
 });
  
+ac.export("map", function(){
+    "use strict";
+
+    var graphics = ac.import('graphics');
+
+    var _mapObject = {
+        grid: [],
+        elem: undefined,
+
+        setTile: function(image, x, y){
+            //position in the tileset image
+            var t = ac.TILESIZE;
+            this.grid[y][x] = 1;
+            this.canvas.draw(image, 0, 0, x*t, y*t);
+        },
+
+        render: function(grid){
+            // render grid
+        }
+    };
+
+    return {
+        create: function(cols, rows){
+            var t = ac.TILESIZE;
+            var map = $.extend(true, {}, _mapObject);
+
+            for (var i = 0; i < rows; i++) {
+                map.grid.push([]);
+                for (var j = 0; j < cols; j++) {
+                    map.grid[i].push({id: 0});
+                }
+            }
+            map.canvas = graphics.createCanvas(cols * t, rows * t);
+            map.elem = $('<div/>').addClass('.map');
+            map.elem.append(map.canvas.elem);
+            return map;
+        },
+
+        init: function(modules){
+            var self = this;
+        }
+    };
+});
+ 
 ac.export("widget", function(){
     "use strict";
 
@@ -382,50 +426,6 @@ ac.export("editor", function(){
 
     return {
         init: init
-    };
-});
- 
-ac.export("map", function(){
-    "use strict";
-
-    var graphics = ac.import('graphics');
-
-    var _mapObject = {
-        grid: [],
-        elem: undefined,
-
-        setTile: function(image, x, y){
-            //position in the tileset image
-            var t = ac.TILESIZE;
-            this.grid[y][x] = 1;
-            this.canvas.draw(image, 0, 0, x*t, y*t);
-        },
-
-        render: function(grid){
-            // render grid
-        }
-    };
-
-    return {
-        create: function(cols, rows){
-            var t = ac.TILESIZE;
-            var map = $.extend(true, {}, _mapObject);
-
-            for (var i = 0; i < rows; i++) {
-                map.grid.push([]);
-                for (var j = 0; j < cols; j++) {
-                    map.grid[i].push({id: 0});
-                }
-            }
-            map.canvas = graphics.createCanvas(cols * t, rows * t);
-            map.elem = $('<div/>').addClass('.map');
-            map.elem.append(map.canvas.elem);
-            return map;
-        },
-
-        init: function(modules){
-            var self = this;
-        }
     };
 });
  

11f730269ff746f3f95308e8f6a75fc3d5b6155f
Karlisson Bezerra <contact@hacktoon.com>
2016-06-10 00:48:49 -0300

    Add DOM module to interface with jquery

---
+++ src/base/dom.js
@@ -0,0 +1,31 @@
+
+ac.export("dom", function(env){
+    "use strict";
+
+    var get = function(selector){
+        return $(selector);
+    };
+
+    var append = function(target, child){
+        target.append(child);
+    };
+
+    var create = function(tag, options){
+        var elem = $("<"+tag+"/>");
+        return elem;
+    };
+
+    var action = function(type, target, callback){
+
+        target.on(type, function(e){
+            callback(e);
+        });
+    };
+
+    return {
+        append: append,
+        action: action,
+        create: create,
+        get: get
+    };
+});

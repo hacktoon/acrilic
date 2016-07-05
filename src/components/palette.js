660f0a469aee06af5c6a8e79f86c3e52d736a387
Karlisson Bezerra <contact@hacktoon.com>
2016-07-04 22:30:42 -0300

    Isolate palette container creation

--- src/components/palette.js
+++ src/components/palette.js
@@ -5,17 +5,15 @@ ac.export("palette", function(env){
     var $tileset = ac.import("tileset"),
         $widget  = ac.import("widget");
 
-    var tile_bts = {},
-        element;
+    var tile_bts = {};
 
     var selectTile = function(id){
         env.set("CURRENT_TILE", tile_bts[id]);
         tile_bts[id].select();
     };
 
-    var createTileButtons = function(palette_selector, tileset){
+    var createTileButtons = function(tileset){
         var buttons = [];
-
         tileset.forEach(function(tile, _){
             var tile_bt = $widget.createTileButton(tile, function(){
                 env.get("CURRENT_TILE").unselect();
@@ -24,11 +22,12 @@ ac.export("palette", function(env){
             buttons.push(tile_bt.render());
             tile_bts[tile.id] = tile_bt;
         });
-        element = $widget.createContainer(palette_selector, buttons);
+        return buttons;
     };
 
     var createPalette = function(palette_selector, tileset) {
-        createTileButtons(palette_selector, tileset);
+        var buttons = createTileButtons(tileset);
+        $widget.createContainer(palette_selector, buttons);
         selectTile(0);
     };
 
